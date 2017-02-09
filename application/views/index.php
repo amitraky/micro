85a017bebd6d1ebaec629a4a8bd76c20a9452fcb
amitraky@gmail.com <amitraky@gmail.com>
2017-02-09 18:21:10 +0530

    add new validation only number functon

--- application/views/index.php
+++ application/views/index.php
@@ -43,6 +43,20 @@ function auto_logout() {
 
 
 </script>
+
+
+<script>
+//onkeypress="return IsNumeric(event);"
+	
+	    var specialKeys = new Array();
+        specialKeys.push(8); //Backspace
+        function IsNumeric(e) {
+            var keyCode = e.which ? e.which : e.keyCode
+            var ret = ((keyCode >= 48 && keyCode <= 57) || specialKeys.indexOf(keyCode) != -1);
+            document.getElementById("error").style.display = ret ? "none" : "inline";
+            return ret;
+        }
+	</script>
     <!-- Bootstrap core CSS -->
     <?php 
 	 
