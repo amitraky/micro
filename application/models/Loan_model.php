a68a2e58dcdaa8f57c33d094529d3447892b83b5
amitraky@gmail.com <amitraky@gmail.com>
2017-02-11 18:42:02 +0530

    add loan model

---
+++ application/models/Loan_model.php
@@ -0,0 +1,46 @@
+<?php
+	class Loan_model extends CI_Model
+	{
+		
+		public function __construct()
+		{
+			parent::__construct();
+		}
+		
+		// get all users list
+		public function get_users()
+		{
+		  $this->db->order_by('or_lid','DESC');
+		 return  $this->db->get('view_users')->result();
+		
+		}
+		
+		
+		// get all users list
+		public function user()
+		{
+		 $reg = 1;
+		 
+		 if(uri(3))
+		   $reg= uri(3);
+		 
+		 return  $this->db->where('t1_1_userid',$reg)->get('view_users')->row();
+		
+		}
+		
+		public function setting()
+		{
+		return $this->db->get('m00_setconfig')->result();
+		}
+		
+		public function users()
+		{
+		  return $this->db->get('view_users')->result();
+		}
+		
+		
+		
+		
+		
+			
+}
